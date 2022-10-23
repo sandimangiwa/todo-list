c73eade57d526cd95875d2c0d2b2d0b7f8cd1aee
sandimangwia <pongsinaran@gmail.com>
2022-10-24 05:56:20 +0700

    fix:test title

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,19 @@
 <template>
-  <q-layout view="lHh Lpr lFf" class="bg-grey-2">
-    <header
-      class="shadow-custome q-mb-lg q-py-lg bg-primary text-white"
-      data-cy="header-background"
-    >
-      <div class="container">
-        <q-toolbar-title
-          class="text-h5 text-bold text-uppercase q-py-sm"
-          data-cy="header-title"
-        >
-          To Do List App
-        </q-toolbar-title>
-      </div>
-    </header>
+  <q-layout view="lHh Lpr lFf">
     <q-page-container class="bg-grey-2">
+      <div
+        class="shadow-custome q-py-lg bg-primary text-white"
+        data-cy="header-background"
+      >
+        <div class="container">
+          <span
+            data-cy="header-title"
+            class="text-h5 text-bold text-uppercase q-py-sm"
+          >
+            TO DO LIST APP
+          </span>
+        </div>
+      </div>
       <router-view />
     </q-page-container>
   </q-layout>
